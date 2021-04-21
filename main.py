125006654739fb5e382fc6318d12012f0b7a0a4a
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-04-21 09:31:20 -0400

    add mersenne prime checker

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import numpy as np
+
 start = 3
-stop = 100000
+stop = 3
+
+def mersenne(a=0, b=8, c=1):
+    mrange = 2 ** np.arange(a, b, c, dtype=object) - 1
+    print(mrange)
+    return mrange
 
 def prime(num):
     for d in range(2, num):
@@ -7,7 +14,11 @@ def prime(num):
             return False
     return True
 
-print(2)
-for n in range(start, stop, 2):
-    if prime(n):
-        print(n)
+def check(numbers):
+    print(2)
+    for n in numbers:
+        if prime(n):
+            print(n)
+        print('Checked '+str(n))
+
+check(mersenne(a=16, b=31, c=1))
